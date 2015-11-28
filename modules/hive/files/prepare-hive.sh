ea4ac52b9d17afe6127cce98ae9a4a6e41fe157b
Aleksei <me.aleksei@gmail.com>
2015-11-28 14:41:12 -0800

    Change dir permissions for Hive

--- modules/hive/files/prepare-hive.sh
+++ modules/hive/files/prepare-hive.sh
@@ -4,4 +4,9 @@
 
 export HDFS_USER=hdfs
 
-su - $HDFS_USER -c "$HADOOP_PREFIX/bin/hadoop fs -mkdir -p /tmp /user/hive/warehouse; $HADOOP_PREFIX/bin/hadoop fs -chmod g+w /tmp /user/hive/warehouse"
+su - $HDFS_USER -c "\
+$HADOOP_PREFIX/bin/hdfs dfs -mkdir -p /tmp /user/hive/warehouse;\
+$HADOOP_PREFIX/bin/hdfs dfs -chmod g+w /tmp /user/hive/warehouse;\
+$HADOOP_PREFIX/bin/hdfs dfs -chmod -R u+rwx,g+rwx,o+rx /user;\
+$HADOOP_PREFIX/bin/hdfs dfs -chmod -R u+rwx,g+rwx,o+rx /tmp;\
+"
